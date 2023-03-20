f3c7924a394613090c635ac43aa4efaba3910b03
Tzvi (Steve) Neiman <tneiman19@gmail.com>
2023-03-20 18:27:23 -0400

    Moved password to a config file and referencing the value in the server.js file

--- server.js
+++ server.js
@@ -1,15 +1,17 @@
 const cTable = require("console.table");
 const express = require("express");
 const mysql = require("mysql2");
+const workbenchPwd = require("./config");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+console.log(workbechPwd);
 // create the connection to database
 const connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: workbenchPwd,
   database: "company",
 });
 
